6e97a92ea63e5243dcb804157296890f70b0bc0a
shijian <jian.shi@ys100.com>
2025-03-17 13:36:53 +0800

    refactor(hooks/useModal): 优化 useModal 钩子的代码结构
    - 重新格式化代码，提高可读性
    - 将 content 参数更名为 Content，以符合驼峰命名规范
    - 移除不必要的空格和换行符，使代码更加紧凑

--- src/hooks/useModal/index.ts
+++ src/hooks/useModal/index.ts
@@ -1,12 +1,12 @@
 import { Modal } from "ant-design-vue";
 import { DefineComponent } from "vue";
-export const useModal = (content: DefineComponent<{}, {}, any>) => {
-    const openModal = async () => {
-        Modal.confirm({
-            modalRender: () => h(content),
-        })
-    };
-    return {
-        openModal
-    }
+export const useModal = (Content: DefineComponent<{}, {}, any>) => {
+  const openModal = async () => {
+    Modal.confirm({
+      modalRender: () => h(Content),
+    });
+  };
+  return {
+    openModal,
+  };
 };
