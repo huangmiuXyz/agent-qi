eb6e40afa4c950d1fc4f75f6fdaede00d1e69cfa
shijian <jian.shi@ys100.com>
2025-03-18 14:34:08 +0800

    refactor(decorators): 移除 AIF 功能装饰器中的无用代码
    - 删除了 AIF 函数中不再使用的 console.log 语句
    - 简化了代码结构，提高了代码可读性和性能

--- src/decorators/AIFunctionDecorators.ts
+++ src/decorators/AIFunctionDecorators.ts
@@ -1,8 +1,6 @@
 interface AIFOptions { }
 export const AIF = <T extends (...args: any[]) => any>(options: AIFOptions, func: T) => {
     return (...args: Parameters<T>): ReturnType<T> => {
-        console.log(Object.getOwnPropertyDescriptors(func));
-
         return func(...args);
     }
 }
